771b6aef100f55b36609dafec4d9466fa49a2177
MuteMini <63823931+MuteMini@users.noreply.github.com>
2020-06-06 13:03:32 -0400

    messing around with praw

--- test.py
+++ test.py
@@ -1,19 +1,36 @@
 import praw
 
 def getRedditClient():
+    redditLink = "https://www.reddit.com/r/Jokes/comments/gxmqa6/mens_help_line_letter_of_the_month/"
+    redditPost = Submission.id_from_url(redditLink)
+    print(Submission.id_from_url(redditLink))
+    
     reddit = praw.Reddit(client_id="MuFZGhJium139Q",
                      client_secret="JCGwE1FgXThG1DYlHZnEcwj1WN4",
                         user_agent="validdit page checker")
-    submission = reddit.submission(id="gx8t25")
-    print(submission.author)
+    submission = reddit.submission(id=redditPost)
+    print(submission.title)
+    #print(vars(submission))
+    
+    author = submission.author
+    print(author.name)
+    print(author.comment_karma)
+    print(author.link_karma)
+    print(author.has_verified_email)
+
+    subreddit = submission.subreddit
+    print("r/"+subreddit.display_name)
+    print(subreddit.quarantine)
+    
     print(submission.title)
     print(submission.score)
-    print(submission.upvote_ratio)
-    print(submission.url)
-    print(submission.subreddit)
+    downvoteNum = (1-submission.upvote_ratio)
+    print(round(downvoteNum*submission.score))
+    url = submission.url if submission.url != redditLink
+    print(url)
     print(submission.num_comments)
     #only gets the top level comments - ones without parent comments, the starter of the comment chain.
-    for comment in submission.comments:
-        print(comment.body)
+    #for comment in submission.comments:
+    #   print(comment.body)
 
 getRedditClient()
