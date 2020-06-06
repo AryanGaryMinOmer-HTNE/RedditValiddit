7fef2049622806696b669d76ff09641994505f24
Gary-Qin <52765139+Gary-Qin@users.noreply.github.com>
2020-06-06 18:13:51 -0400

    Update garytest.py

--- garytest.py
+++ garytest.py
@@ -8,23 +8,47 @@ submission = reddit.submission(url='https://www.reddit.com/r/news/comments/gxqkj
 all_comments = submission.comments.list()
 
 #news sources from https://blog.feedspot.com/usa_news_websites/ and https://blog.feedspot.com/canadian_news_websites/
-#30 news sources listed
+#30 news sources listed + reddit + wikipedia
 news_sources = ["cnn.com", "nytimes.com", "huffpost.com", "foxnews.com", "usatoday.com", 
                 "reuters.com", "politico.com", "yahoo.com", "npr.org", "latimes.com",
                 "brietbart.com", "nypost.com", "nbcnews.com", "abcnews.go.com", "cbsnews.com",
                 "cbc.ca", "theglobeandmail.com", "ctvnews.ca", "globalnews.ca", "thestar.com",
                 "huffingtonpost.ca", "nationalpost.com", "torontosun.com", "financialpost.com", "vancouversun.com",
-                "macleans.ca", "montrealgazette.com", "citynews.ca", "metronews.ca", "calgaryherald.com"]
+                "macleans.ca", "montrealgazette.com", "citynews.ca", "metronews.ca", "calgaryherald.com",
+                "reddit.com", "wikipedia.org"]
 
 comments_with_links = []
 
+pprint.pprint(vars(all_comments[62]))
+print()
+
+
 for i in range(len(all_comments)):
-    a = i
-    cur_comment = vars(all_comments[i])["body_html"]
+    # a = i
+
+    try:
+        cur_comment = vars(all_comments[i])["body_html"]
+    except KeyError:
+        cur_comment = ""
+    else:
+        cur_comment = vars(all_comments[i])["body_html"]
+
     start_of_link = 0 
     end_of_link = 0
     pointer = 0
-    
+
+    # try:
+    #     cur_comment.index("<a href", pointer)
+    # except ValueError:
+    #     start_of_link = 0
+    #     end_of_link = 0
+    # else:
+    #     start_of_link = cur_comment.index("<a href=\"", pointer) + 9
+    #     end_of_link = cur_comment.index("\">", start_of_link)
+    #     print(cur_comment[start_of_link:end_of_link])
+    #     print(i)
+    #     print()
+
     while pointer < len(cur_comment):
         try:
             cur_comment.index("<a href", pointer)
