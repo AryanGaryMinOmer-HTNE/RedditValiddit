e3cdd246e263c2d5712e98b5c6ff7e5ddb414dd7
Gary-Qin <52765139+Gary-Qin@users.noreply.github.com>
2020-06-06 15:39:48 -0400

    Update garytest.py

--- garytest.py
+++ garytest.py
@@ -3,9 +3,14 @@ import praw
 reddit = praw.Reddit(client_id="MuFZGhJium139Q",
                         client_secret="JCGwE1FgXThG1DYlHZnEcwj1WN4",
                         user_agent="validdit page checker")
+newsSubreddit = reddit.subreddit("news")
 
 print(reddit.read_only)
 
-for submission in reddit.subreddit("news").hot(limit = 10):
-    print(submission.title)
+for submission in newsSubreddit.hot(limit = 10):
+    print(submission.title)  # Output: the submission's title
+    print(submission.score)  # Output: the submission's score
+    print(submission.id)     # Output: the submission's ID
+    print(submission.url)    # Output: the URL the submission points to
+    print()
 
