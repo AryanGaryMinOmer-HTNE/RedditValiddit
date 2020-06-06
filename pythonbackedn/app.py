42661e42936fceaa56a7ee17e2ded8dcd0a3af7f
omeradeel26 <57784751+omeradeel26@users.noreply.github.com>
2020-06-06 15:24:31 -0400

    Added python flask backend

---
+++ pythonbackedn/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+import praw 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('input.html')
+
+@app.route('/search', methods=['POST'])
+def getRedditClient():
+    redditlink = request.form['link']
+    redditID = praw.models.Submission.id_from_url(redditlink)
+    reddit = praw.Reddit(client_id="MuFZGhJium139Q",
+                        client_secret="JCGwE1FgXThG1DYlHZnEcwj1WN4",
+                        user_agent="validdit page checker")
+    submission = reddit.submission(id=redditID)
+    return render_template('search.html', titl=submission.title, auth=submission.author, up=submission.ups, dow=submission.downs)
