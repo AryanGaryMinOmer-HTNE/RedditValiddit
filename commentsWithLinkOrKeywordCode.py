756b53859aeaef12fda65a32a800c47bbb681289
Gary-Qin <52765139+Gary-Qin@users.noreply.github.com>
2020-06-06 20:14:09 -0400

    finished

--- commentsWithLinkOrKeywordCode.py
+++ commentsWithLinkOrKeywordCode.py
@@ -5,7 +5,7 @@ reddit = praw.Reddit(client_id="MuFZGhJium139Q",
                     user_agent="validdit page checker")
 
 submission = reddit.submission(url='https://www.reddit.com/r/news/comments/gxqkji/washington_dc_expects_citys_largest_protest_since/')
-all_comments = submission.comments.list()
+all_comments = list(submission.comments)
 
 #news sources from https://blog.feedspot.com/usa_news_websites/ and https://blog.feedspot.com/canadian_news_websites/
 #30 news sources listed + reddit
@@ -16,10 +16,10 @@ news_sources = ["cnn.com", "nytimes.com", "huffpost.com", "foxnews.com", "usatod
                 "huffingtonpost.ca", "nationalpost.com", "torontosun.com", "financialpost.com", "vancouversun.com",
                 "macleans.ca", "montrealgazette.com", "citynews.ca", "metronews.ca", "calgaryherald.com",
                 "reddit.com"]
-keywords = ["fake", ""]
+keywords = ["fake", "accurate", "biased", "radical", "politic", "made-up", "corrupt", "racist"]
 
-comments_with_links = [] # holds the comments and links
-comments_with_keywords = []
+comments_with_links = [] # holds the comments, links, start and end index of link, and karma
+comments_with_keywords = [] # holds the comments, keyword found, start and end index of link, and karma
 
 # loop going through every comment
 for i in range(len(all_comments)):
@@ -32,6 +32,8 @@ for i in range(len(all_comments)):
 
     start_of_link = 0 
     end_of_link = 0
+    start_of_word = 0
+    end_of_word = 0
     pointer = 0
 
     # try/catch for locating comments with article/reddit links
@@ -52,7 +54,26 @@ for i in range(len(all_comments)):
                 clean_body = clean_body.replace("(" + cur_comment[start_of_link:end_of_link] + ")", "")
                 clean_body = clean_body.replace('\n', ' ')
                 
-                comments_with_links.append([clean_body, cur_comment[start_of_link:end_of_link]])
+                comments_with_links.append([clean_body, cur_comment[start_of_link:end_of_link], start_of_link, end_of_link, vars(all_comments[i])["score"]])
+    
+    # try/catch for locating comments containing key words
+    for j in range(len(keywords)):
+        try:
+            cur_comment.index(keywords[j])
+        except ValueError:
+            start_of_word = 0
+            end_of_word = 0
+        else:
+            start_of_word = cur_comment.index(keywords[j])
+            end_of_word = start_of_word + len(keywords[j])
+
+            clean_body = vars(all_comments[i])["body"]
+            clean_body = clean_body.translate({ord('['): None})
+            clean_body = clean_body.translate({ord(']'): None})
+            clean_body = clean_body.replace("(" + cur_comment[start_of_link:end_of_link] + ")", "")
+            clean_body = clean_body.replace('\n', ' ')
+            comments_with_keywords.append([clean_body, keywords[j], start_of_word, end_of_word, vars(all_comments[i])["score"]])
+
 
 
         
